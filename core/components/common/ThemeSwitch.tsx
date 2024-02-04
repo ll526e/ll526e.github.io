0029ca6056f645ae1dbee144afa5ef965fbb7295
ll526e <ll526e@163.com>
2024-02-04 19:21:54 +0800

    feat: some change

--- core/components/common/ThemeSwitch.tsx
+++ core/components/common/ThemeSwitch.tsx
@@ -3,28 +3,9 @@ import { Svg } from './Svg'
 
 export const ThemeSwitch = () => {
   const { state: { theme }, dispatch } = useApp()
-  const themeToken = useCookie.get<'light' | 'dark'>('theme')
-  const themeMedia = window.matchMedia("(prefers-color-scheme: light)")
-
-  useEffect(() => {
-    regTheme(themeMedia);
-    themeMedia.addEventListener('change', regTheme);
-    return () => {
-      themeMedia.removeEventListener('change', () => { });
-    }
-  }, []);
-
-  const regTheme = (media: MediaQueryListEventInit) => {
-    const str = themeToken || (media.matches ? 'light' : 'dark')
-    dispatch({
-      type: 'change_theme', payload: {
-        theme: str
-      }
-    })
-  }
 
   const onThemeChange = () => {
-    let str = theme === 'light' ? 'dark' : 'light'
+    const str = theme === 'light' ? 'dark' : 'light'
     useCookie.set('theme', str)
     dispatch({
       type: 'change_theme', payload: {
